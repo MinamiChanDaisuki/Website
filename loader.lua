6dc2352dd1cd3489a64054c9a78e25553e5f2747
美波 <anatawahitoka@gmail.com>
2025-08-18 21:57:41 +0900

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,25 @@
-getgenv().script_key=script_key
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ba63e5108c182e73a0d0ab53587b6439.lua"))()
+local api = loadstring(game:HttpGet("https://sdkapi-public.luarmor.net/library.lua"))();
+api.script_id = "ba63e5108c182e73a0d0ab53587b6439";
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer;
+local status_api = api.check_key(script_key);
+
+if not script_key then
+    player:Kick("[ERROR] Authentication Failed - Key not found");
+    return;
+end;
+
+local handlers = {
+    KEY_VALID = function()
+        pcall(api.purge_cache);
+        api.load_script();
+    end,
+    KEY_HWID_LOCKED = function() player:Kick("[WARNING] HWID Locked - Reset your HWID") end,
+    KEY_EXPIRED = function() player:Kick("[ERROR] Key Expired - Subscription ended") end,
+    KEY_BANNED = function() player:Kick("[ERROR] Key Blacklisted - Access revoked") end,
+    KEY_INCORRECT = function() player:Kick("[ERROR] Invalid Key - Your Cooked") end
+};
+
+(handlers[status_api.code] or function()
+    player:Kick("[ERROR] Authentication Error -", status_api.message);
+end)();
